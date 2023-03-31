b5ea6df4b93ddf63e1e773c2a3cba3d7691bc6c2
Isotoxal <62714538+IsotoxalDev@users.noreply.github.com>
2023-03-31 19:09:08 +0530

    Types 🩹

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,87 @@ mod board;
 mod game;
 mod input;
 mod menu;
-mod types;
 
 use board::*;
 use game::*;
 use input::*;
 use menu::*;
-use types::*;
+
+/// Enum for different parts of snake.
+#[derive(Copy, Clone, PartialEq)]
+pub enum SnakePart {
+    Body,
+    Head,
+    Tail,
+}
+
+/// Enum for different types of states a cell can have
+#[derive(Copy, Clone, PartialEq)]
+pub enum CellState {
+    Empty,
+    Apple,
+    Snake(SnakePart, Direction),
+}
+
+/// Enum for state of game.
+#[derive(Copy, Clone, PartialEq)]
+pub enum GameState {
+    Playing,
+    Paused,
+    GameOver,
+}
+
+/// Type export to easily create an array of CellState
+pub type CellData = [[CellState; SIZE.0]; SIZE.1];
+
+/// Enum for direction
+#[derive(Copy, Clone, PartialEq)]
+pub enum Direction {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
+impl Direction {
+    /// Get opposite direction
+    pub fn opposite(&self) -> Self {
+        match self {
+            Self::Up => Self::Down,
+            Self::Down => Self::Up,
+            Self::Right => Self::Left,
+            Self::Left => Self::Right,
+        }
+    }
+    /// get the next block based on the give coordinates
+    pub fn next_block(&self, x: usize, y: usize) -> (usize, usize) {
+        let (mut temp_x, mut temp_y) = match self {
+            Direction::Up => (0, -1),
+            Direction::Down => (0, 1),
+            Direction::Right => (1, 0),
+            Direction::Left => (-1, 0),
+        };
+        temp_x = x as isize + temp_x;
+        temp_y = y as isize + temp_y;
+
+        // Loop the snake if out of bounds
+        temp_x = if temp_x > SIZE.0 as isize - 1 {
+            temp_x - SIZE.0 as isize
+        } else if temp_x < 0 {
+            temp_x + SIZE.0 as isize
+        } else {
+            temp_x
+        };
+        temp_y = if temp_y > SIZE.1 as isize - 1 {
+            temp_y - SIZE.1 as isize
+        } else if temp_y < 0 {
+            temp_y + SIZE.1 as isize
+        } else {
+            temp_y
+        };
+        (temp_x as usize, temp_y as usize)
+    }
+}
 
 #[component]
 fn Snake(cx: Scope) -> impl IntoView {
